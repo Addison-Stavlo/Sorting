137bf54c367390008aa68163f5d01cece2bd50bb
Addison-Stavlo <AddieStavlo@gmail.com>
2019-03-27 16:16:39 -0400

    merge_sort first take, merge function is incorrect

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -24,21 +24,55 @@ def selection_sort(arr):
                 smallest_index = j
         # TO-DO: swap
         arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
-        print(arr)
     return arr
 
-
-print(selection_sort([5, 3, 7, 1, 9]))
 
 # TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
+    madeSwap = True
+
+    while madeSwap:
+        madeSwap = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                madeSwap = True
 
     return arr
 
 
+print(bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
 
-    return arr
+
+def count_sort(arr, maximum=-1):
+    maximum = max(arr)
+
+    sorted_arr = [0]*len(arr)
+    counts = [0]*(maximum+1)
+
+    # store amount of times we see each element in array
+    for element in arr:
+        counts[element] += 1
+
+    # modify counts to make each count the sum of previous counts before it
+    sum_of_counts = 0
+    for i in range(len(counts)):
+        sum_of_counts += counts[i]
+        counts[i] = sum_of_counts
+
+    # build output
+    for i in range(len(arr)):
+        # set location of element arr[i]
+        # to be at the index of how many elements we counted before it
+        sorted_arr[counts[arr[i]] - 1] = arr[i]
+        # we placed one instance of that number we counted
+        # subtract one so next time we insert it, it inserts 1 space to the left
+        counts[(arr[i])] -= 1
+
+    return sorted_arr
+
+
+print(count_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,33 +1,59 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+def merge(arrA, arrB):
+    merged_arr = []
     # TO-DO
-    
+    for i in range(min([len(arrA), len(arrB)])):
+        if arrA[i] < arrB[i]:
+            merged_arr.append(arrA[i])
+            merged_arr.append(arrB[i])
+        else:
+            merged_arr.append(arrB[i])
+            merged_arr.append(arrA[i])
+
+    # compensate for lists not being same length
+    difference = len(arrA) - len(arrB)
+    if difference > 0:
+        # add the remaining amount of arrA
+        merged_arr += arrA[-difference:]
+    elif difference < 0:
+        merged_arr += arrB[difference:]
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    if len(arr) > 1:
+        left = arr[:len(arr)//2]
+        right = arr[len(arr)//2:]
 
+        arr1 = merge_sort(left)
+        arr2 = merge_sort(right)
+
+        arr = merge(arr1, arr2)
     return arr
 
 
+print(merge([3, 5, 7, 8, 9], [1, 2, 6, 10]))
+print(merge_sort([5, 6, 4, 3, 8, 9, 0, 1, 2, 3, 5, 3, 7]))
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
-
-    return arr
 
 
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+# def merge_in_place(arr, start, mid, end):
+#     # TO-DO
 
-    return arr
+#     return arr
+
+
+# def merge_sort_in_place(arr, l, r):
+#     # TO-DO
+
+#     return arr
+
+
+# # STRETCH: implement the Timsort function below
+# # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+# def timsort(arr):
+
+#     return arr
