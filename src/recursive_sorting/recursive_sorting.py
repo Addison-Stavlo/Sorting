e7016bd37d6a9bc8a8d41703c63f62788fa9bdc5
Addison-Stavlo <AddieStavlo@gmail.com>
2019-03-28 16:30:26 -0400

    in-place merge sort complete...

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -34,21 +34,47 @@ def merge_sort(arr):
 
 # STRETCH: implement an in-place merge sort algorithm
 
+# JUST USE MERGE FUNCTION USED IN RECURSIVE ONE!
+# WE DONT NEED THESE EXTRA INPUT GARBAGES
+def merge_in_place(arr, start, mid, end):
+    # TO-DO
 
-# def merge_in_place(arr, start, mid, end):
-#     # TO-DO
-
-#     return arr
+    return arr
 
 
-# def merge_sort_in_place(arr, l, r):
-#     # TO-DO
+# i dont want l or r as inputs, thats stupid
+# these are things we can tell from receiving arr
+# making them required inputs is redundant
+# im setting them equal to empty lists instead of removing for test file purposes
+def merge_sort_in_place(arr, l=[], r=[]):
+    # TO-DO
+    if len(arr) < 2:
+        return arr
 
-#     return arr
+    arr_split = []
+    # split array into list of divided parts
+    for item in arr:
+        arr_split.append([item])
+
+    # while we are still divided
+    while len(arr_split[0]) < len(arr):
+        i = 0
+        # loop through and merge adjacent elements
+        while i < len(arr_split) - 1:
+            merged_section = merge(arr_split[i], arr_split[i+1])
+            # replace i-th element with merged section
+            arr_split[i] = merged_section
+            # delete i+1-th element now that it is in i-th
+            del arr_split[i+1]
+            i += 1
+
+    return arr_split[0]
 
 
 # # STRETCH: implement the Timsort function below
 # # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-# def timsort(arr):
 
-#     return arr
+
+def timsort(arr):
+
+    return arr
